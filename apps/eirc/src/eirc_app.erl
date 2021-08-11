8cb831be3af9e3c28aba0d6c8a3bd07b57dbe993
Jim Moen <LnJimMoen@outlook.com>
2021-08-11 20:46:09 +0800

    Use the rebar3 build tool.

---
+++ apps/eirc/src/eirc_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc eirc public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(eirc_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    eirc_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
